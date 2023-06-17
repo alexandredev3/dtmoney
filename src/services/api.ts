cd514415ec580d3636bb1cb7cf96ec8bafd3639a
Alexandre Costa <costasalexandre511@gmail.com>
2023-06-17 14:46:35 +0000

    add api REACT_APP_URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://alexandredev3-potential-space-rotary-w5qw79v9v69c5j4-3000.preview.app.github.dev/api'
+  baseURL: process.env.REACT_APP_URL
 });
