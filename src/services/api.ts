ae2e65f2623b5a8deeebf5bc98bb39987f66600f
Alexandre Costa <costasalexandre511@gmail.com>
2023-06-17 15:44:55 +0000

    fix api service VERCEL_URL env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-console.log(process.env.VERCEL_URL)
-
 export const api = axios.create({
-  baseURL: "https://" + process.env.VERCEL_URL + "/api"
+  baseURL: "https://" + process.env.REACT_APP_VERCEL_URL + "/api"
 });
